60fa6b4552268de9e602862ac09de24dd9034c65
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-15 12:16:13 +0700

    Nomor 6 - Coloring a Moveable Circle
    Nomor 6 - Coloring a Moveable Circle

---
+++ Week 1/APPL1_W1_N6/src/Nomor6/MoveCircle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Nomor6;
+
+/**
+ *
+ * Author = Bima Putra S | 191524038 | D4-1B "2019
+ * Politeknik Negeri Bandung
+ * Tanggal : 15 Maret 2021
+ * Deskripsi : Nomor 6 - Coloring a Movable Circle
+ */
+
+import java.awt.*;
+import javax.swing.*;
+public class MoveCircle{
+    //-----------------------------------
+    // Set up a frame for the GUI.
+    //-----------------------------------
+    public static void main(String[] args){
+        JFrame frame = new JFrame ("MoveCircle");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(400,300));
+        frame.getContentPane().add(new CirclePanel(400,300));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
