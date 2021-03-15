538e2bf5d012b0ab2fb3c7759088501cd0dd547f
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-15 11:10:03 +0700

    Nomor 1 - Finish
    - Commision.java
    1. Tambah Class Commision,
    2. Tambah Constructor Commision panggil Super + commsRate,
    3. Method addSales,
    4. Method pay,
    5. Method toString,
    
    - Staff.java
    1. Tambah dictonary Staff

---
+++ Week 1/APPL1_Week1_Nomor1/src/Nomor1/Commission.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Nomor1;
+
+/**
+ *
+ * Author = Bima Putra S | 191524038 | D4-1B "2019
+ * Politeknik Negeri Bandung
+ * Tanggal :
+ * Deskripsi :
+ */
+
+public class Commission extends Hourly{
+    private double totSales;
+    private double commsRate;
+    
+    public Commission(String eName, String eAddress, String ePhone, String socSecNumber, double rate, double cRate) {
+        super(eName, eAddress, ePhone, socSecNumber, rate);
+        cRate = commsRate;
+    }
+    
+    public void addSales(double totalSales){
+        totSales = totalSales;
+    }
+    
+    @Override
+    public double pay(){
+        double temp = totSales * commsRate;
+        totSales = 0;
+        return super.pay() + temp;
+    }
+    
+    public String toString(){
+        String result = super.toString();
+        result += "\nTotal sales: " + totSales;
+        return result;
+    }
+    
+}
