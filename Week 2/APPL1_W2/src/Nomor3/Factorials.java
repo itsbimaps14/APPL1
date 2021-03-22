0c971f0b22bdfe83a002e4566819e98f64186c25
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-22 11:28:16 +0700

    Script Nomor 3
    Script Nomor 3

---
+++ Week 2/APPL1_W2/src/Nomor3/Factorials.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Nomor3;
+
+/**
+ *
+ * Author = Bima Putra S | 191524038 | D4-1B "2019
+ * Politeknik Negeri Bandung
+ * Tanggal : 22 Maret 2021
+ * Deskripsi : Throwing Exceptions - Nomor 3
+ */
+
+import java.util.Scanner;
+
+public class Factorials {
+    public static void main(String[] args){
+        String keepGoing = "y";
+        Scanner scan = new Scanner(System.in);
+        
+        while (keepGoing.equals("y") || keepGoing.equals("Y")){
+        System.out.print("Enter an integer: ");
+            int val = scan.nextInt();
+            System.out.println("Factorial(" + val + ") = " + MathUtils.factorial(val));
+            System.out.print("Another factorial? (y/n) ");
+            keepGoing = scan.next();
+        }
+    }
+}
