2557f9ba914c66c28e448e20350a530e53b543b4
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-22 11:17:45 +0700

    Nomor 2 - Selesai
    Nomor 2 - Selesai

--- Week 2/APPL1_W2/src/Nomor2/ParseInts.java
+++ Week 2/APPL1_W2/src/Nomor2/ParseInts.java
@@ -10,9 +10,32 @@ package Nomor2;
  *
  * Author = Bima Putra S | 191524038 | D4-1B "2019
  * Politeknik Negeri Bandung
- * Tanggal :
- * Deskripsi :
+ * Tanggal : 22 Maret 2021
+ * Deskripsi : Placing Exception Handlers - Nomor 2
  */
-public class ParseInts {
 
+import java.util.Scanner;
+
+public class ParseInts {
+    public static void main(String[] args){
+        int val, sum=0;
+        Scanner scan = new Scanner(System.in);
+        String line;
+        
+        System.out.println("Enter a line of text");
+        Scanner scanLine = new Scanner(scan.nextLine());
+        
+        while (scanLine.hasNext()){
+            val = Integer.parseInt(scanLine.next());
+            sum += val;
+            try{
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+            }
+            catch (Exception e){
+                //Do Nothing
+            }
+        }
+        System.out.println("The sum of the integers on this line is " + sum);
+    }
 }
