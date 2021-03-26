e4e38442253ff1c51fd3700c139f551b3b0233de
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-26 15:11:47 +0700

    a Currency Converter (Nomor 7) - Finish

--- Week 2/APPL1_W2/src/Nomor7/RatePanel.java
+++ Week 2/APPL1_W2/src/Nomor7/RatePanel.java
@@ -17,12 +17,15 @@ package Nomor7;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.text.DecimalFormat;
 import javax.swing.*;
 
 public class RatePanel extends JPanel{
     private double[] rate; // exchange rates
     private String[] currencyName;
-    private JLabel result;
+    private JLabel result, inputLabel;
+    JComboBox currencyList;
+    JTextField textField;
     
     // ------------------------------------------------------------
     // Sets up a panel to convert cost from one of 6 currencies
@@ -32,6 +35,7 @@ public class RatePanel extends JPanel{
     // ------------------------------------------------------------
     public RatePanel (){
         JLabel title = new JLabel ("How much is that in dollars?");
+        title.setBounds(200, 100, 50, 50);
         title.setAlignmentX (Component.CENTER_ALIGNMENT);
         title.setFont (new Font ("Helvetica", Font.BOLD, 20));
         
@@ -39,9 +43,23 @@ public class RatePanel extends JPanel{
         currencyName = new String[] {"Select the currency..", "European Euro", "Canadian Dollar", "Japanese Yen", 
             "Australian Dollar", "Indian Rupee", "Mexican Peso"};
         rate = new double [] {0.0, 1.2103, 0.7351, 0.0091, 0.6969, 0.0222, 0.0880};
+        
+        currencyList = new JComboBox(currencyName);
+        currencyList.setLocation(200, 150);
+        
+        inputLabel = new JLabel("Amount of Money you Want to Convert");
+        
+        textField = new JTextField(20);
+        textField.addActionListener(new ComboListener());
+        
         result = new JLabel (" ------------ ");
-        add (title);
-        add (result);
+        result.setLocation(300, 300);
+        
+        add(title);
+        add(currencyList);
+        add(result);
+        add(inputLabel);
+        add(textField);
     }
     
     // ******************************************************
@@ -53,9 +71,21 @@ public class RatePanel extends JPanel{
         // the value in that currency then computes and
         // displays the value in U.S. Dollars.
         // --------------------------------------------------
+        @Override
         public void actionPerformed (ActionEvent event){
-            int index = 0;
-            result.setText ("1 " + currencyName[index] + " = " + rate[index] + " U.S. Dollars");
+            double val = 0;
+            DecimalFormat decimal = new DecimalFormat("#.##");
+            
+            try{
+                val = Double.parseDouble(textField.getText());
+            }
+            catch(Exception e){
+                result.setText("Please Input a Number");
+            }
+            
+            int index = currencyList.getSelectedIndex();
+            
+            result.setText (val + currencyName[index] + " = " + decimal.format(rate[index] * val) + " U.S. Dollars");
         }
     }
 }
