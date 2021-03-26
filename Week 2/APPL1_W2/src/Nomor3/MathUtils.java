ed17a706991d475b5c10e381a9e5ba865de835e9
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-26 20:20:14 +0700

    Perbaikan Nomor 3 + Penambahan Komentar Pada Script
    Perbaikan Nomor 3 + Penambahan Komentar Pada Script

--- Week 2/APPL1_W2/src/Nomor3/MathUtils.java
+++ Week 2/APPL1_W2/src/Nomor3/MathUtils.java
@@ -19,17 +19,21 @@ public class MathUtils {
     // Returns the factorial of the argument given
     //-------------------------------------------------------------
     public static int factorial(int n){
+        // Aturan 2 dan 3
         if(n>=0 && n <=16){
+            // Aturan 1 (karena 0 tidak masuk ke fungsi 
+            // for maka nilai hasil default 1)
             int fac = 1;
             for (int i=n; i>0; i--)
-            fac *= i;
+                fac *= i;
             return fac;
         }
         else if(n>16){
             throw new IllegalArgumentException("Maximum Integer is 16");
         }
         else {
-            throw new  IllegalArgumentException("Factorial is not available for negative integers");
+            throw new  IllegalArgumentException("Factorial is not available "
+                    + "for negative integers");
         }
     }
 } 
