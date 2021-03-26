809dfce1de031a76584fe5e48a07b74405b87746
itsbimaps14 <61398214+itsbimaps14@users.noreply.github.com>
2021-03-26 15:50:50 +0700

    Script Nomor 5 (Reading From and Writing to Text Files)

---
+++ Week 2/APPL1_W2/src/Nomor5/Warning.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Nomor5;
+
+/**
+ *
+ * Author = Bima Putra S | 191524038 | D4-1B "2019
+ * Politeknik Negeri Bandung
+ * Tanggal : 26 Maret 2021
+ * Deskripsi : Reads student data from a text file and writes data to another text file.
+ */
+
+public class Warning {
+    
+}
